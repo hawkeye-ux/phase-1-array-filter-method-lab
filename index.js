8515bcfc8af1924722784a21e677566f4f89cf4c
hawkeye-ux <110018080+hawkeye-ux@users.noreply.github.com>
2022-08-30 18:17:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arrayOfDriversNames, string) {
+    return arrayOfDriversNames.filter((el) => el.toLowerCase().includes(string.toLowerCase()));
+}
+
+function fuzzyMatch(arrayOfDriversNames, string) {
+    return arrayOfDriversNames.filter((el) => el.toLowerCase().startsWith(string.toLowerCase()));
+}
+
+function matchName(arrayOfDriversObjects, string) {
+    return arrayOfDriversObjects.filter((driver) => driver.name.toLowerCase() == string.toLowerCase());
+}
